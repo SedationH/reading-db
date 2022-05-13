9ccc481d79b85cafbcc0e15f081b1091b58ed09d
SedationH <sedationh@gmail.com>
2022-05-13 10:55:21 +0800

    增加链接

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 function App() {
   return (
     <div className="App">
-      <a href="/AtomicHabits/index.html">AtomicHabits</a>
+      <ul>
+        <li>
+          <a href="/AtomicHabits/index.html">AtomicHabits</a>
+        </li>
+        <li>
+          <a href="/Counselling-for-Toads-A-Psychological-Adventure/index.html">
+            Counselling-for-Toads-A-Psychological-Adventure
+          </a>
+        </li>
+      </ul>
     </div>
   )
 }
